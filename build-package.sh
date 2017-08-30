86950188316281db8fe639fe32028ee2da4818ac
Stephen <stephengroat@users.noreply.github.com>
2017-08-29 17:14:31 -0700

    Update build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -435,7 +435,7 @@ termux_step_start_build() {
 		DEBCONF_FRONTEND=noninteractive \
 			APT_CONFIG="Dir::Etc::Parts ${TERMUX_PREFIX}/etc/apt/apt.conf.d/; Dir::Etc::main ${TERMUX_PREFIX}/etc/apt/apt.conf;" \
 			apt-get $TERMUX_APT upgrade
-		cat /var/log/dpkg.log
+		/usr/bin/dpkg --force-not-root --force-architecture --admindir=/data/data/com.termux/files/usr/var/lib/dpkg --status-fd 13 --no-triggers --unpack --auto-deconfigure /data/data/com.termux/files/usr/var/cache/apt/archives/busybox_1.27.1-2_aarch64.deb /data/data/com.termux/files/usr/var/cache/apt/archives/libandroid-support_22_aarch64.deb /data/data/com.termux/files/usr/var/cache/apt/archives/command-not-found_1.25_aarch64.deb /data/data/com.termux/files/usr/var/cache/apt/archives/ncurses_6.0.20170827_aarch64.deb 
 		sudo chown -R builder:builder /data
 		while IFS=',' read -ra PKG; do
 			for p in "${PKG[@]}"; do

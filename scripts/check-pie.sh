3a6ec5eda4a04d68b1c765d5337571f941e84f5b
Stephen G <stephen@egroat.com>
2017-10-27 13:53:20 -0700

    Move scripts to submodule

--- scripts/check-pie.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# check-pie.sh - script to detect non-PIE binaries (which does not work on Android)
-
-cd /data/data/com.termux/files/usr/bin/
-for file in *; do
-	if readelf -h $file 2>/dev/null | grep -q 'Type:[[:space:]]*EXEC'; then
-		echo $file
-	fi
-done

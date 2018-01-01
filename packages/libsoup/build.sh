043b386b85980e26829cc161dd0a4d998d4eeb13
Stephen <stephengroat@users.noreply.github.com>
2017-12-31 17:10:20 -0800

    add devpackage depends to libsoup

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_SHA256=2aa7556b16563279875a525b1dd37b94ac055e01fddf8e4c9695c57e829c6b
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libsoup/${TERMUX_PKG_VERSION:0:4}/libsoup-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-vala --without-gssapi"
 TERMUX_PKG_DEPENDS="glib, libsqlite, libxml2"
+TERMUX_PKG_DEVPACKAGE_DEPENDS="glib-dev, libsqlite-dev, libxml2-dev"
